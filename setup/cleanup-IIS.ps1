13740b07070623218970142dad0e7a5bedee6e95
Mark Gray <mark@grayszone.com>
2015-10-27 11:15:07 -0700

    created cleanup scripts for Azure VMs

---
+++ setup/cleanup-IIS.ps1
@@ -0,0 +1,15 @@
+﻿#############################################################################
+# Script to return server to baseline for Demo
+#############################################################################
+
+#Remove Petshop website directory
+del C:\inetpub\MSPetShop -Recurse -Force
+
+#Remove MSPetshop website
+Remove-Website 'MSPetShop'
+
+#Remove petshop source code
+del c:\data\petshop.zip
+
+#Set default website back to port 80
+Set-WebBinding -Name 'Default Web Site' -BindingInformation '*:8080:' -PropertyName Port -Value 80
---
+++ setup/cleanup-sql.ps1
@@ -0,0 +1,15 @@
+﻿#############################################################################
+# Script to return server to baseline for Demo
+#############################################################################
+
+#Remove all PetShop Databases
+dir SQLSERVER:\SQl\dd-sql\default\databases | where name -Like 'MSPetShop*' | Remove-Item -Verbose
+
+#Remove all PetShop DB accounts
+cd SQLSERVER:\SQl\dd-sql\default\logins 
+Remove-Item 'mspetshop' -Verbose
+
+#Remove all cache files
+Remove-Item 'C:\ProgramData\CacheScript.txt' -Force -Verbose
+Remove-Item 'C:\ProgramData\*.sql' -Force -Verbose
+Remove-Item 'C:\Program Files\Microsoft SQL Server\SqlFiles' -Recurse -Force -Verbose
