2a0cbc9638197d54ad00054aa50abd83a17719cb
Mark Gray <mark@grayszone.com>
2015-10-27 22:55:54 -0700

    Removed extra 't' in Assert-PetShoptInDev and added verbose to cleanup

--- setup/cleanup-IIS.ps1
+++ setup/cleanup-IIS.ps1
@@ -3,13 +3,13 @@
 #############################################################################
 
 #Remove Petshop website directory
-del C:\inetpub\MSPetShop -Recurse -Force
+del C:\inetpub\MSPetShop -Recurse -Force -Verbose
 
 #Remove MSPetshop website
-Remove-Website 'MSPetShop'
+Remove-Website 'MSPetShop' -Verbose
 
 #Remove petshop source code
-del c:\data\petshop.zip
+del c:\data\petshop.zip -Verbose
 
 #Set default website back to port 80
-Set-WebBinding -Name 'Default Web Site' -BindingInformation '*:8080:' -PropertyName Port -Value 80
+Set-WebBinding -Name 'Default Web Site' -BindingInformation '*:8080:' -PropertyName Port -Value 80 -Verbose
