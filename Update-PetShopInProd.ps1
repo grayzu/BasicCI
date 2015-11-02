0940c80753b2b03dcb15d888f5c2af180f6bdf5a
Mark Gray <mark@grayszone.com>
2015-11-02 08:43:43 -0800

    Added script for remote updating Prod Machines

---
+++ Update-PetShopInProd.ps1
@@ -0,0 +1,6 @@
+﻿$sqlCon = New-CimSession -ComputerName dd-sql.westus.cloudapp.azure.com -Credential $cred
+$webCon = New-CimSession -ComputerName dd-web.westus.cloudapp.azure.com -Credential $cred
+
+# Force udate of configurations on prod machines
+Update-DscConfiguration -CimSession $sqlCon -Wait -Verbose
+Update-DscConfiguration -CimSession $webCon -Wait -Verbose
